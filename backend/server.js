b57e6fd0a0293408a4f4ed8b44fa8786e7a5782a
zukurnaz <77931192+zukurnaz@users.noreply.github.com>
2021-03-21 20:23:03 +0300

    Add server boilerplate

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port : ${port}`)
+});
